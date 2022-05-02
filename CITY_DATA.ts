70c9df9aee6bb3df9b166ddae848c1e9606a1cbe
bodrius <bogdanpechenyk@gmail.com>
2022-05-02 11:00:05 +0300

    feat(data): create example

---
+++ CITY_DATA.ts
@@ -0,0 +1,9 @@
+// DOR DATA USE THIS LINK https://github.com/leonid-shevtsov/ua-cities/blob/master/ua-cities.xml
+
+export const DataCity = [
+  {
+    Id: 1,
+    Name: "cities.Alupka",
+    Value: "cities.Alupka",
+  },
+];
